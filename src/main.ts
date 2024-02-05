c39e2aed30f403dae5c65fbdf7e8c5b9b7ea2fbd
CAIBITV <yyt14588@gmail.com>
2024-02-05 16:41:23 +0800

    feat: 环境变量的配置

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import App from '@/App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // 配置element-plus的国际化
-//@ts-ignore 忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+//ts-expect-error
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
 app.use(ElementPlus, {
   locale: zhCn,
 })
+
+console.log(import.meta.env)
+
 app.mount('#app')
