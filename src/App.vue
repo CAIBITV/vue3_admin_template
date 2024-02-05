c3f9d8b0f2ebdaa07dd26e757fd36366702e771c
CAIBITV <yyt14588@gmail.com>
2024-02-05 14:40:56 +0800

    feat: 配置prettier、stylelint

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>
+  <div class="box">
     <h1>Welcome to your Vue 3 App!</h1>
     <p>Start building your awesome application here.</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+defineOptions({
+  name: 'App',
+})
+const str = 'Hello, Vue 3!'
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400pox;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
 </style>
