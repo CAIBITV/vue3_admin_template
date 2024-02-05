39a4ffe35ddc14e4f8abf75ae1255ec7b755150a
CAIBITV <yyt14588@gmail.com>
2024-02-05 14:11:35 +0800

    eslint配置

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome to your Vue 3 App!</h1>
+    <p>Start building your awesome application here.</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+
+</style>
