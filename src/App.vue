a1ca9151805808b7569fb69b4fdf41c1617da2ce
CAIBITV <yyt14588@gmail.com>
2024-02-05 16:10:53 +0800

    feat: 继承element-plus

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
   <div class="box">
     <h1>Welcome to your Vue 3 App!</h1>
     <p>Start building your awesome application here.</p>
+    <el-button type="primary" :icon="Plus">Hello, Element!</el-button>
+    <el-button type="success" :icon="Edit">Hello, Element!</el-button>
+    <el-pagination
+      v-model:current-page="currentPage1"
+      :page-size="100"
+      :small="small"
+      :disabled="disabled"
+      :background="background"
+      layout="total, prev, pager, next"
+      :total="1000"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
@@ -9,7 +22,22 @@
 defineOptions({
   name: 'App',
 })
-const str = 'Hello, Vue 3!'
+
+import { Plus, Edit } from '@element-plus/icons-vue'
+
+import { ref } from 'vue'
+
+const currentPage1 = ref(5)
+const small = ref(false)
+const background = ref(false)
+const disabled = ref(false)
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`)
+}
 </script>
 
 <style scoped lang="scss">
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 引入element-plus及样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 配置element-plus的国际化
+//@ts-ignore 忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-const fun = () => {
-  console.log('这时没有格式化的代码！！！')
-}
-fun()
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
