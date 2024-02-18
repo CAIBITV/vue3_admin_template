606ac6d696928a5e3aef2f37399a29ece52b4c09
CAIBITV <yyt14588@gmail.com>
2024-02-18 15:27:30 +0800

    feat: 用户仓库数据ts类型定义与token工具类

--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -8,24 +8,28 @@ export interface loginFormData {
 //定义全部接口返回数据都拥有ts类型
 export interface ResponseData {
   code: number
-  message: string
-  ok: boolean
 }
 
 //定义登录接口返回数据类型
 export interface loginResponseData extends ResponseData {
   data: {
-    token: string
+    token?: string
+    message?: string
   }
 }
 
 //定义获取用户信息返回数据类型
 export interface userInfoReponseData extends ResponseData {
   data: {
-    routes: string[]
-    buttons: string[]
-    roles: string[]
-    name: string
-    avatar: string
+    userId?: number
+    avatar?: string
+    username?: string
+    password?: string
+    desc?: string
+    roles?: string[]
+    buttons?: string[]
+    routes?: string[]
+    token?: string
+    message?: string
   }
 }
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -2,26 +2,31 @@
 import { defineStore } from 'pinia'
 // 引入接口
 import { reqLogin } from '@/api/user'
-import type { loginFormData } from '@/api/user/type'
+// 引入数据类型
+import type { loginFormData, loginResponseData } from '@/api/user/type'
+import type { UserState } from '@/store/modules/types/type'
+// 引入操作本地存储的方法
+import { SET_TOKEN, GET_TOKEN } from '@/utils/token'
 
 export const useUserStore = defineStore({
   id: 'user',
-  state: () => ({
-    token: localStorage.getItem('token') // 用户token
+  state: (): UserState => ({
+    token: GET_TOKEN() // 用户token
   }),
   getters: {},
   actions: {
     async userLogin(userInfo: loginFormData) {
       // 登录请求
-      const res: any = await reqLogin(userInfo)
+      const res: loginResponseData = await reqLogin(userInfo)
       // 成功：200 => token
       // 失败：201 => 登录失败错误信息
       if (res.code === 200) {
         // pinia仓库存储一下token
-        this.token = res.data.token
+        this.token = res.data.token as string
+
         // 由于 pinia/vuex 存储数据其实是js对象
         // 本地存储持久化
-        localStorage.setItem('token', res.data.token)
+        SET_TOKEN(res.data.token!)
         // 保证async函数返回一个成功的promise
         return 'ok'
       } else {
