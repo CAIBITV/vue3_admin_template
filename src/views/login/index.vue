472a89cb4eb23f3f739b4000d2c07406d2746fc6
CAIBITV <yyt14588@gmail.com>
2024-02-21 00:08:01 +0800

    feat: 登录模块表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,17 +3,22 @@
     <el-row>
       <el-col :span="12" :xs="0" />
       <el-col :span="12" :xs="24">
-        <el-form class="login_form">
+        <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginFormRef"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
-          <el-form-item>
-            <el-input :prefix-icon="User" v-model="formData.username" />
+          <el-form-item prop="username">
+            <el-input :prefix-icon="User" v-model="loginForm.username" />
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               :prefix-icon="Lock"
               type="password"
-              v-model="formData.password"
+              v-model="loginForm.password"
               show-password
             />
           </el-form-item>
@@ -40,10 +45,27 @@ defineOptions({
 })
 import { User, Lock } from '@element-plus/icons-vue'
 import { reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
 
-const formData = reactive({
+interface LoginForm {
+  username: string
+  password: string
+}
+
+const loginFormRef = ref<FormInstance>()
+const loginForm = reactive<LoginForm>({
   username: 'admin',
   password: '111111'
+})
+const rules = reactive<FormRules<LoginForm>>({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, max: 20, message: '长度在 6 到 20 个字符', trigger: 'blur' }
+  ]
 })
 
 const loading = ref(false)
@@ -59,7 +81,20 @@ const router = useRouter()
 
 import { getTime } from '@/utils/time'
 
+// 保证所有的表单项目验证通过再发送请求
+/* const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+} */
+
 async function login() {
+  await loginFormRef.value!.validate()
   //点击登录按钮以后干什么
   //通知仓库发起请求
   //请求成功->路由跳转
@@ -67,7 +102,7 @@ async function login() {
   try {
     loading.value = true
     // 保证登录成功
-    await userStore.userLogin(formData)
+    await userStore.userLogin(loginForm)
     // 编程式导航
     ElNotification({
       title: `HI,${getTime()}好`,
